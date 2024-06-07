c5dfcece0226f664e49507fafce48163b63ccc0e
LexKorn <fischer84@mail.ru>
2024-06-07 07:30:10 +0300

    chatBlock FE+BE

--- src/components/ChatBlock/ChatBlock.tsx
+++ src/components/ChatBlock/ChatBlock.tsx
@@ -3,59 +3,44 @@ import {Form, Button, ListGroup, Card} from 'react-bootstrap'
 
 import { IMessage } from '../../types/types'
 import { LoaderT } from '../LoaderType/LoaderT'
-import { index } from '../../utils/calc'
+import { createMessage, fetchMessages, deleteMessages } from '../../http/messagesAPI'
 
 import './chatBlock.sass'
 
 
-const answers: string[] = [
-    "Ваше обращение очень важно, мы скоро свяжемся с Вами.",
-    "Ваш запрос принят. Скоро вернёмся к Вам.",
-    "Наши менеджеры уже решают Вашу задачу",
-    "Мы свяжемся с Вами в ближайшее время.",
-    "Рады будем помочь со всеми вашими вопросами."
-];
-
 const ChatBlock: React.FC = () => {
-    const [message, setMessage] = useState<IMessage>({} as IMessage);                    // created by user
-    const [receivedMessage, setReceivedMessage] = useState<IMessage>({} as IMessage);    // created by system
-    const [messages, setMessages] = useState<IMessage[]>([]);                            // array of all messeges
+    const [messages, setMessages] = useState<IMessage[]>([]);
     const [text, setText] = useState<string>('');
     const [typing, setTyping] = useState<boolean>(false);
+    const [toggle, setToggle] = useState<boolean>(false);
 
     useEffect(() => {
-        if (message.id) {
-            setMessages([...messages, message]);
-        }
-    }, [message]);
+        fetchMessages().then(data => setMessages(data));
+    }, [typing, toggle]);
 
-    useEffect(() => {
-        if (receivedMessage.id) {
-            setMessages([...messages, receivedMessage]);
-        }
-    }, [receivedMessage]);
-
-    const createMessage = () => {
+    const onCreateMessage = () => {
         if (!text.trim()) {               
             return alert('Необходимо ввести текст');
         }
-        setMessage({id: Date.now(), userId: 1, text: text});
+        createMessage(text, 'user');
         setText('');
 
-        setTimeout(() => {setTyping(true)}, 1200);
+        setTimeout(() => {setTyping(true)}, 1000);
         setTimeout(() => {setTyping(false)}, 4500);
-        setTimeout(() => {createReceivedMessage()}, 4700);
     };
 
-    const createReceivedMessage = () => {
-        setReceivedMessage({id: Date.now(), userId: 2, text: `${answers[index(answers)]}`});
-    };
+    const onDeleteMessages = () => {
+        if (window.confirm('Вы действительно хотите удалить переписку?')) {
+            deleteMessages();
+            setToggle(prev => !prev);
+        };            
+    }   
 
     //@ts-ignore
     const keyPress = (e: React.KeyboardEvent<FormControlElement>) => {
         if (e.key === 'Enter') {
             e.preventDefault();
-            createMessage();
+            onCreateMessage();
         }
     };
 
@@ -70,7 +55,7 @@ const ChatBlock: React.FC = () => {
                     placeholder={"Набирите сообщение"}
                 />
                 <div className="chat__form_btn">
-                    <Button variant={"outline-warning"} className="chat__btn" onClick={createMessage}>Отправить</Button>              
+                    <Button variant={"outline-warning"} className="chat__btn" onClick={onCreateMessage}>Отправить</Button>
                 </div>
             </Form>
 
@@ -79,13 +64,14 @@ const ChatBlock: React.FC = () => {
                     messages.map(item =>
                         <Card 
                             key={item.id}
-                            className={item.userId === 1 ? "chat__list_item shadow receiver" : "chat__list_item shadow"}
+                            className={item.sender === "user" ? "chat__list_item shadow sender" : "chat__list_item shadow"}
                             >
                             {item.text}
                         </Card>
                 )}
                 {typing && <LoaderT />}
             </ListGroup>
+            <Button variant={"outline-danger"} className="chat__btn_rmv" onClick={onDeleteMessages}>Очистить переписку</Button>
         </div>
     )
 }
